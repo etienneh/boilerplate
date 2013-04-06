13b079874c912aca557f417e86680ef0bad9e582
etienneh <etiennehoullier@gmail.com>
2013-04-06 18:43:36 +0200

    add classic / responsive / mailing & dev

---
+++ development/js/site.js
@@ -0,0 +1,6 @@
+/* console.time('Temps-1');
+   console.timeEnd('Temps-1');
+   
+   console.profile('Profile-1')
+   console.profileEnd('Profile-1')
+   ====================================================== */ 
