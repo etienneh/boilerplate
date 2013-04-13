d6b366701451ff2691edb682af10f49b50d4ad8a
etienneh <etiennehoullier@gmail.com>
2013-04-13 23:24:46 +0200

    empty

--- classic/js/site.js
+++
@@ -1,6 +0,0 @@
-/* console.time('Temps-1');
-   console.timeEnd('Temps-1');
-   
-   console.profile('Profile-1')
-   console.profileEnd('Profile-1')
-   ====================================================== */ 
